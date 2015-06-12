0c6541e3bfd12e251d479bd16bf42bd8f09e574c
Nathan Ross Powell <nathanrosspowell@gmail.com>
2015-06-11 23:06:23 -0400

    Grunt deploy Thu Jun 11 2015 23:06:18

---
+++ js/main.js
@@ -0,0 +1,14 @@
+(function() {
+
+"use-strict";
+
+$('#progressbar').progressbar({
+    warningMarker: 60,
+    dangerMarker: 80,
+    maximum: 100,
+    step: 5
+});
+
+console.log("ERTGHERAG");
+
+})();
