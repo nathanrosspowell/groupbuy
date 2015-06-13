d8add1ca0ae0ab8ede6fd7daf1d02afa26a05670
Nathan Ross Powell <nathanrosspowell@gmail.com>
2015-06-12 23:01:00 -0400

    Color change on JS set percentage value

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,11 @@
-(function() {
-
-"use-strict";
-
-$('.progress .progress-bar').progressbar();
-
-})();
+$(document).ready(function() {
+    "use-strict";
+    var target = 90;
+    $('.progress .progress-bar')
+        .attr('data-transitiongoal', target)
+        .progressbar({
+        update: function(current_percentage, $this) {
+            $this.parent().parent().css('background-color', 'rgb(' + Math.round(current_percentage / 100 * 255) + ', 0, 0)');
+        }
+    });
+})
