f04a4db26c4c25b13fbdb8049474225e6b84ace9
Nathan Ross Powell <nathanrosspowell@gmail.com>
2015-06-15 23:10:41 -0400

    Grunt deploy Mon Jun 15 2015 23:10:29

--- js/main.js
+++ js/main.js
@@ -1,29 +1,39 @@
 $(document).ready(function() {
     "use-strict";
-    var bar1Target = 100, bar1Current = 90, bar1Value = 0
-        bar2Target = 100, bar2Current = 10, bar2Value = 0;
 
-    bar1Value = (bar1Current / bar1Target) * 100;
-    bar2Value = (bar2Current / bar2Target) * 100;
-
-
-    console.log( bar1Value );
-    console.log( bar2Value );
-
-    $('#pb1 .progress-bar')
-        .attr('data-transitiongoal', Math.floor(bar1Value))
-        .progressbar({
-        update: function(current_percentage, $this) {
-            $this.parent().parent().css('background-color', 'rgb(' + Math.round(current_percentage / 100 * 255) + ', 0, 0)');
+    var data = [
+        {
+            target : 100,
+            current : 90
+        },
+        {
+            target : 100,
+            current : 50 
+        },
+        {
+            target : 100,
+            current : 20
         }
+    ];
+        
+    var targetDiv = $('#target-div')
+    data.forEach(function(entry){
+        var value = (entry.current / entry.target) * 100;
+        var well = $('<div/>')
+            .addClass('well-lg')
+            .appendTo(targetDiv);
+        var progress = $('<div/>')
+            .addClass('progress')
+            .appendTo(well);
+        var bar = $('<div/>')
+            .addClass('progress-bar')
+            .addClass('six-sec-ease-in-out')
+            .appendTo(progress);
+        bar.attr('data-transitiongoal', Math.floor(value))
+            .progressbar({
+                update: function(current_percentage, $this) {
+                    $this.parent().parent().css('background-color', 'rgb(' + Math.round(current_percentage / 100 * 255) + ', 0, 0)');
+            }
+        });
     });
-
-    $('#pb2 .progress-bar')
-        .attr('data-transitiongoal', 10 )
-        .progressbar({
-        update: function(current_percentage, $this) {
-            $this.parent().parent().css('background-color', 'rgb(' + Math.round(current_percentage / 100 * 255) + ', 0, 0)');
-        }
-    });
-
 })
