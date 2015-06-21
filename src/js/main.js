4e428fb20f07664003b2b40b548f303c9c44cb1e
Nathan Ross Powell <nathanrosspowell@gmail.com>
2015-06-21 18:58:22 -0400

    Extend jQuery
    Split the functionality into functions.
    Registered the main function with the jQuery object.

--- src/js/main.js
+++ src/js/main.js
@@ -1,39 +1,72 @@
 $(document).ready(function() {
     "use-strict";
 
-    var data = [
-        {
-            target : 100,
-            current : 90
-        },
-        {
-            target : 100,
-            current : 50 
-        },
-        {
-            target : 100,
-            current : 20
-        }
-    ];
-        
-    var targetDiv = $('#target-div')
-    data.forEach(function(entry){
-        var value = (entry.current / entry.target) * 100;
-        var well = $('<div/>')
-            .addClass('well-lg')
-            .appendTo(targetDiv);
-        var progress = $('<div/>')
-            .addClass('progress')
-            .appendTo(well);
-        var bar = $('<div/>')
-            .addClass('progress-bar')
-            .addClass('six-sec-ease-in-out')
-            .appendTo(progress);
-        bar.attr('data-transitiongoal', Math.floor(value))
-            .progressbar({
-                update: function(current_percentage, $this) {
-                    $this.parent().parent().css('background-color', 'rgb(' + Math.round(current_percentage / 100 * 255) + ', 0, 0)');
-            }
+    // Turn the array of tier data into object with all the information.
+    function MakeGroupBuyData( barArray ){
+        var data = {
+            bars : barArray,
+            totalJoined : 0,
+            totalCommited: 0,
+            currentPrice : 0,
+            neededForNextPrice : 0,
+            neededForLowestPrice : 0
+        };
+        return data;
+    }
+    
+    // Turn the information into a HTML on the specified div.
+    function WriteGroupBuyData( targetDiv, groupBuyData ){
+        groupBuyData.bars.forEach(function(bar){
+            var value = (bar.joined / bar.quantity) * 100;
+            var well = $('<div/>')
+                .addClass('well-lg')
+                .appendTo(targetDiv);
+            var progress = $('<div/>')
+                .addClass('progress')
+                .appendTo(well);
+            var bar = $('<div/>')
+                .addClass('progress-bar')
+                .addClass('six-sec-ease-in-out')
+                .appendTo(progress);
+            bar.attr('data-transitiongoal', Math.floor(value))
+                .progressbar({
+                    update: function(joined_percentage, $this) {
+                        var color = Math.round(joined_percentage / 100 * 255);
+                        $this.parent().parent().css('background-color', 'rgb(' + color + ', 0, 0)');
+                }
+            });
         });
+    }
+
+    // Extend the jQuerey object to be able to use 'groupbuy'.
+    jQuery.fn.extend({
+        groupbuy: function(options) {
+            return this.each(function() {
+                WriteGroupBuyData( this, MakeGroupBuyData(options));
+            });
+        }
     });
+    
+    // Debug test.
+    $('#target-div').groupbuy([
+            {
+                quantity : 100,
+                price : 99.99,
+                joined : 90,
+                commited : 10
+            },
+            {
+                quantity : 200,
+                price : 79.99,
+                joined : 50,
+                commited : 10
+            },
+            {
+                quantity : 400,
+                price : 59.99,
+                joined : 20,
+                commited : 10
+            }
+        ]
+    );
 })
