41bec341f00bfbdff4abdde17e073bece8a5f13c
Nathan Ross Powell <nathanrosspowell@gmail.com>
2015-06-22 23:54:51 -0400

    Move committed down the tiers if there is overflow

--- src/js/groupbuy.js
+++ src/js/groupbuy.js
@@ -10,17 +10,29 @@ function MakeGroupBuyData( tierArray ){
         neededForNextPrice : 0,
         neededForLowestPrice : 0
     };
-    // Loop all the tiers,
+    // Loop all the tiers backwards:
+    //     * if any of commited overflows, put it into the next tier
+    var extras = 0;
+    for (i = tierArray.length - 1; i > -1; --i) {
+        var tier = tierArray[i];
+        console.log(extras);
+        tier.commited += extras;
+        extras = 0;
+        if ( tier.commited > tier.quantity ) {
+            extras = tier.commited - tier.quantity;
+            tier.commited = tier.quantity
+        }
+    }
+    // Loop all the tiers forward:
     //     * if the joined and commited make the total
     //     * all overflow become joined in the next tier
-    var extras = 0;
-    for (i = 0; i < tierArray.length; i++) {
+    extras = 0;
+    for (i = 0; i < tierArray.length; ++i) {
         var tier = tierArray[i];
         tier.joined += extras;
         extras = 0;
         var total = tier.joined + tier.commited;
-        if ( total >= tier.quantity )
-        {
+        if ( total >= tier.quantity ) {
             extras = total - tier.quantity;
             tier.joined = tier.quantity;
             tier.commited = 0;
